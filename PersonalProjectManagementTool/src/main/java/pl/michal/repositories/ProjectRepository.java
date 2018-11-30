a0c0e2def68fac92a0ec922e0e4dc814b921bdd9
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-30 23:49:14 +0100

    added two methods that should override, those methods perform searching by projectIdentifer and findAll

--- PersonalProjectManagementTool/src/main/java/pl/michal/repositories/ProjectRepository.java
+++ PersonalProjectManagementTool/src/main/java/pl/michal/repositories/ProjectRepository.java
@@ -3,9 +3,12 @@ package pl.michal.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import pl.michal.entity.Project;
+import pl.michal.entities.Project;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
+	Project findByProjectIdentifer(String projectId);
 	
+	@Override
+	Iterable<Project> findAll();
 }
