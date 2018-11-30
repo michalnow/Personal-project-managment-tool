2843b646a443e9e55cd38adbce4157f7c471c9bb
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-30 23:42:55 +0100

    renamed package

---
+++ PersonalProjectManagementTool/src/main/java/pl/michal/entities/Project.java
@@ -0,0 +1,123 @@
+package pl.michal.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class Project {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Project name is required")
+	private String projectName;
+	
+	@NotBlank(message = "Project Identifier is required")
+	@Size(min=4, max=5, message="four or five characters are required")
+	@Column(updatable=false, unique = true)
+	private String projectIdentifer;
+	
+	@NotBlank(message = "description is required")
+	private String description;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date startDate;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date endDate;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date createdAt;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date updatedAt;
+
+	public Project() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectIdentifer() {
+		return projectIdentifer;
+	}
+
+	public void setProjectIdentifer(String projectIdentifer) {
+		this.projectIdentifer = projectIdentifer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+}
