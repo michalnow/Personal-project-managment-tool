2ae72cf2fc841101474f0a670bc08c7edd4c2f8f
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-30 16:15:33 +0100

    initialization of repo

---
+++ PersonalProjectManagementTool/src/main/java/pl/michal/PersonalProjectManagementToolApplication.java
@@ -0,0 +1,12 @@
+package pl.michal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectManagementToolApplication.class, args);
+	}
+}
