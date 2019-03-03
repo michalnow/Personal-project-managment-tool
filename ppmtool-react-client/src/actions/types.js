52c48ced13847ff8f3d5d45aea1293d33a05090b
Michał Nowakowski <michal951@poczta.onet.pl>
2019-03-03 18:23:31 +0100

    created project action, secuirty actions and types

--- ppmtool-react-client/src/actions/types.js
+++ ppmtool-react-client/src/actions/types.js
@@ -1,1 +1,6 @@
 export const GET_ERRORS = "GET_ERROS";
+export const GET_PROJECTS = "GET_PROJECTS";
+export const GET_PROJECT = "GET_PROJECT";
+export const DELETE_PROJECT = "DELETE_PROJECT";
+
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
