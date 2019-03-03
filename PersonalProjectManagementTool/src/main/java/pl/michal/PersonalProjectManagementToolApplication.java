96eb22269b1266f4083565a3e24b586c1c777786
Michał Nowakowski <michal951@poczta.onet.pl>
2019-03-03 18:11:39 +0100

    create BCryptPasswordEncoder bean

--- PersonalProjectManagementTool/src/main/java/pl/michal/PersonalProjectManagementToolApplication.java
+++ PersonalProjectManagementTool/src/main/java/pl/michal/PersonalProjectManagementToolApplication.java
@@ -2,9 +2,16 @@ package pl.michal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PersonalProjectManagementToolApplication {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PersonalProjectManagementToolApplication.class, args);
