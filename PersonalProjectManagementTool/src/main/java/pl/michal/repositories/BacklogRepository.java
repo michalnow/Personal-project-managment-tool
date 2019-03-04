d46312036cce42e5f0c9483ba9b3ebacaa96bff0
Michał Nowakowski <michal951@poczta.onet.pl>
2019-03-04 12:55:55 +0100

    Create backlog repo and add findByProjectIdentifier

---
+++ PersonalProjectManagementTool/src/main/java/pl/michal/repositories/BacklogRepository.java
@@ -0,0 +1,11 @@
+package pl.michal.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.michal.entities.Backlog;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String identifier);
+}
